939e1deabd6c36caf99ff27ebe39b4615b4569df
rudraa-ksh <rudrak04sh@gmail.com>
2024-11-09 14:39:26 +0530

    Connectivity

--- app/src/main/java/com/example/lodgify/MainActivity.java
+++ app/src/main/java/com/example/lodgify/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lodgify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    public void next(View view){
+        ImageView arrowImage = findViewById(R.id.imageView2);
+
+        arrowImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+            }
         });
     }
 }
